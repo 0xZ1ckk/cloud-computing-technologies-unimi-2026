8cc6b6ad7c3a7f06276815a5d86a99fb925b3374
0xZ1ckk <omarpasculli2001@gmail.com>
2026-02-27 17:02:40 +0100

    Add initial files

---
+++ src/test/java/com/unimi/ticketing_system/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimi.ticketing_system.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
